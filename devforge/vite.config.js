617e3ad7f4673042c7947d0bace72cfa97da8bee
JasonRosalinda <jason.rosalinda@mims.com>
2026-03-04 23:03:10 +0800

    feat: Add Vite configuration with React, PWA, and path aliases.

--- devforge/vite.config.js
+++ devforge/vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/devForge/',
+  base: '/devforge/',
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
     manifest: {
